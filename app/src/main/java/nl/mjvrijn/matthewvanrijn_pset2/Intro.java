7abaae84063a80975f753a1a14e4286b13101497
Matthew van Rijn <matthew.van.rijn@gmail.com>
2016-09-15 17:28:23 +0200

    Improved Story and experimented with multiple activities

--- app/src/main/java/nl/mjvrijn/matthewvanrijn_pset2/Intro.java
+++ app/src/main/java/nl/mjvrijn/matthewvanrijn_pset2/Intro.java
@@ -1,7 +1,9 @@
 package nl.mjvrijn.matthewvanrijn_pset2;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 import java.util.Scanner;
 
@@ -21,5 +23,10 @@ public class Intro extends AppCompatActivity {
         }
 
         System.out.println(story.getStory());
+    }
+
+    public void nextStep(View view) {
+        Intent intent = new Intent(Intro.this, Words.class);
+        startActivity(intent);
     }
 }
